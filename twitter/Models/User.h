e42825eda8eaa202a3cc30c2e6de0c36f8a79010
Githel Lynn Suico <githelsuico@fb.com>
2020-06-29 13:09:25 -0700

    User Story 1 completed

---
+++ twitter/Models/User.h
@@ -0,0 +1,25 @@
+//
+//  User.h
+//  twitter
+//
+//  Created by Githel Lynn Suico on 6/29/20.
+//  Copyright © 2020 Emerson Malca. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface User : NSObject
+
+// MARK: Properties
+@property (nonatomic, strong) NSString *name;
+@property (nonatomic, strong) NSString *screenName;
+
+// MARK: Methods
+// Create initializer
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary;
+
+@end
+
+NS_ASSUME_NONNULL_END
