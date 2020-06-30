9af70e9a60b1c2e1be50e755536394388ab7b936
Githel Lynn Suico <githelsuico@fb.com>
2020-06-29 21:01:53 -0700

    User story 4: compose tweet finished, continue Compose Protocol

---
+++ twitter/View Controllers/ComposeViewController.m
@@ -0,0 +1,81 @@
+//
+//  ComposeViewController.m
+//  twitter
+//
+//  Created by Githel Lynn Suico on 6/29/20.
+//  Copyright © 2020 Emerson Malca. All rights reserved.
+//
+
+#import "ComposeViewController.h"
+#import "Tweet.h"
+#import "APIManager.h"
+
+@interface ComposeViewController ()
+@property (weak, nonatomic) IBOutlet UITextView *tweetView;
+@property (nonatomic, strong) NSString *tweet;
+@end
+
+@implementation ComposeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void) postTweet{
+    [[APIManager shared] postStatusWithText:self.tweet completion:^(Tweet *tweet, NSError *error){
+        if(tweet){
+            UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Tweet Successfully Posted"
+                                                                           message:@""
+                                                                    preferredStyle:(UIAlertControllerStyleAlert)];
+            UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK"
+                                                               style:UIAlertActionStyleDefault
+                                                             handler:^(UIAlertAction * _Nonnull action) {}];
+            [alert addAction:okAction];
+            [self presentViewController:alert animated:YES completion:nil];
+        } else {
+            UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Cannot Post Tweet"
+                                                                           message:@"Network Error"
+                                                                    preferredStyle:(UIAlertControllerStyleAlert)];
+            UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK"
+                                                               style:UIAlertActionStyleDefault
+                                                             handler:^(UIAlertAction * _Nonnull action) {}];
+            [alert addAction:okAction];
+            [self presentViewController:alert animated:YES completion:nil];
+        }
+    }];
+}
+
+- (IBAction)closeTapped:(id)sender {
+    [self dismissViewControllerAnimated:true completion:nil];
+}
+
+- (IBAction)tweetTapped:(id)sender {
+    NSString *text = self.tweetView.text;
+    if([text length] != 0){
+        self.tweet = text;
+        [self postTweet];
+        [self dismissViewControllerAnimated:true completion:nil];
+    } else {
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Cannot Post Tweet"
+                                                                       message:@"Must be more than 0 characters."
+                                                                preferredStyle:(UIAlertControllerStyleAlert)];
+        UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK"
+                                                           style:UIAlertActionStyleDefault
+                                                         handler:^(UIAlertAction * _Nonnull action) {}];
+        [alert addAction:okAction];
+        [self presentViewController:alert animated:YES completion:nil];
+    }
+}
+
+
+/*
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
+
+@end
